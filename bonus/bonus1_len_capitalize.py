90dbad0bdc59e1a511756aabc158ed4b87648df1
Gregory Leck <greg@gregoryleck.com>
2024-10-23 14:55:47 -0400

    Initial commit.

---
+++ bonus/bonus1_len_capitalize.py
@@ -0,0 +1,8 @@
+text = input("Enter a title: ")
+
+length = len(text)
+
+print("The length of the title:", length)
+
+name = input("Enter your name: ")
+print(name.capitalize())
---
+++ bonus/bonus2_zip_rootfolders.py
@@ -0,0 +1,7 @@
+contents = ["doc", "report", "presentation"]
+filenames = ["doc.txt", "report.txt", "presentation.txt"]
+
+for content, filename in zip(contents, filenames):
+    file = open(f"../files/{filename}", "w")
+    file.write(content)
+
